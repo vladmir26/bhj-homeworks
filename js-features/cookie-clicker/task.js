fa3b38d5e27bf322e55a87d724200defb99b1ee8
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-05 17:22:20 +0400

    Попытался привести к числу

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,13 @@
+"use srict"
+
 let clicker = document.querySelector("#clicker__counter");
 let cookie = document.querySelector("#cookie");
 
 function game () {
 
- let a = clicker__counter.textContent += 1; 
- clicker = a;
+let a = Number(clicker__counter.textContent) + 1; 
+
+clicker = a;
 
  }
 
