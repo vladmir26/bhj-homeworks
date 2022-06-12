7ef96b581f4813860fe9a1904401ee7c65ae1e43
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-12 22:51:10 +0400

    Третье задание

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,17 +1,22 @@
-let holes = document.querySelectorAll("hole");
-let hasMole = document.querySelector("hole_has-mole");
+let holes = document.querySelectorAll(".hole");
+let hasMole = document.querySelector(".hole_has-mole");
 let dead = document.querySelector("#dead");
 let lost = document.querySelector("#lost");
 
-for(let hole of holes) {
-    click(hole)
+
+for (let hole of holes) {
+    hole.addEventListener("click", mole)
 }
 
-if (hasMole) {
+
+
+function mole () {
+if (holes) {
  let c = Number(dead.textContent) + 1;
  dead.textContent = c;
 }
-else {
-    let c = Number(lost.textContent) + 1;
-    lost.textContent = c;
+else  {
+    let d = Number(lost.textContent) + 1;
+    lost.textContent = d;
    }
+}
