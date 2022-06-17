339209a42d0b8fb135780bfab2e13569597c0f90
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-17 14:31:46 +0400

    Сделал задание с окнами

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,25 +1,22 @@
 let modalMain = document.querySelector("#modal_main");
-let modalClose = document.querySelector(".modal__close");
+let modalClose = document.querySelectorAll(".modal__close");
 let showSuccess = document.querySelector(".show-success");
 let modalSuccess = document.querySelector("#modal_success");
 
-
 modalMain.classList.add("modal_active");
 
-modalClose.addEventListener("click", removeClassMain);
-
-function removeClassMain () {
-   modalMain.classList.remove("modal_active");
+for (let close of modalClose) {
+    close.addEventListener("click", removeClass);
 }
+
+function removeClass () {
+   modalMain.classList.remove("modal_active");
+   modalSuccess.classList.remove("modal_active");
+}
+
 
 showSuccess.addEventListener("click", addClassSuccess);
 
 function addClassSuccess () {
    modalSuccess.classList.add("modal_active");
 } 
-
-modalClose.addEventListener("click", removeClassSuccess);
-
-function removeClassSuccess () {
-    modalSuccess.classList.remove("modal_active");
-}
