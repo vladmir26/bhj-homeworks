2b81f5c5bf155eea290a85a01a6b19bc07729010
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-26 14:24:35 +0400

    Третье задание

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,28 +5,25 @@ for (let ar of arrows) {
     ar.addEventListener("click", switching);
 }
 
-
-
 function switching (event) {
-    let currentSlider = sliderItem.findIndex((index) => {return index.classList.contains("slider__item_active")}); 
+let currentSlider = sliderItem.findIndex((index) => {return index.classList.contains("slider__item_active")}); 
 
 let nextSlider;
 
-    if (event.target.classList.contains("slider__arrow_next")) {
-       nextSlider = currentSlider + 1;
+if (event.target.classList.contains("slider__arrow_next")) {
+    nextSlider = currentSlider + 1;
     sliderItem[nextSlider].classList.add('slider__item_active');
 }
 if (event.target.classList.contains("slider__arrow_prev")) {
-  nextSlider = currentSlider - 1; 
-  sliderItem[nextSlider].classList.remove('slider__item_active');
+   nextSlider = currentSlider - 1; 
+   sliderItem[nextSlider].classList.remove('slider__item_active');
 } 
+if (nextSlider == sliderItem.length) {
+   currentSlider =  sliderItem[0];
 }
-
-if(nextSlider == sliderItem.length)
-currentSlider =  sliderItem[0];
-
-if(nextSlider < 0) {
+if (nextSlider < 0) {
     currentSlider = sliderItem.length - 1;
 }
 
+}
 
