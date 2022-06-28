b1649b6707d80400f0733f3d6024c797cc39b311
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-28 22:32:56 +0400

    Первое задание

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+let dropdownList = document.querySelector(".dropdown__list");
+let dropdownValue = document.querySelector(".dropdown__value");
+let dropdownLink = document.querySelectorAll(".dropdown__link");
+
+addEventListener("click", openList);
+
+function openList () {
+    dropdownList.classList.toggle("dropdown__list_active");
+}
+
+for (let link of dropdownLink) {
+    link.addEventListener("click", choose);
+}
+
+function choose (event) {
+    dropdownValue.textContent = event.target.textContent;
+    dropdownList.classList.remove("dropdown__list_active");
+}
+
