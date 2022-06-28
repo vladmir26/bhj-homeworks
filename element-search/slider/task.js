84d3687b42c67c802afb24e1d0ac15f11e0cfbb4
vlad_mir <mironow.slava2012@yandex.ru>
2022-06-28 18:35:44 +0400

    Сделанное третье задание

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,24 +6,26 @@ for (let ar of arrows) {
 }
 
 function switching (event) {
-let currentSlider = sliderItem.findIndex((index) => {return index.classList.contains("slider__item_active")}); 
+    let currentSlider = sliderItem.findIndex((index) => {return index.classList.contains("slider__item_active")}); 
 
-let nextSlider;
+    let nextSlider;
 
-if (event.target.classList.contains("slider__arrow_next")) {
+    if (event.target.classList.contains("slider__arrow_next")) {
     nextSlider = currentSlider + 1;
-    sliderItem[nextSlider].classList.add('slider__item_active');
-}
-if (event.target.classList.contains("slider__arrow_prev")) {
+    if (nextSlider === sliderItem.length) {
+        nextSlider =  0;
+        }
+    }
+    if (event.target.classList.contains("slider__arrow_prev")) {
    nextSlider = currentSlider - 1; 
-   sliderItem[nextSlider].classList.remove('slider__item_active');
-} 
-if (nextSlider == sliderItem.length) {
-   currentSlider =  sliderItem[0];
-}
-if (nextSlider < 0) {
-    currentSlider = sliderItem.length - 1;
-}
+   if (nextSlider < 0) {
+       nextSlider = sliderItem.length - 1;
+       }
+    } 
 
-}
+    sliderItem[nextSlider].classList.add('slider__item_active');
+    sliderItem[currentSlider].classList.remove('slider__item_active');
+} 
+
+
 
