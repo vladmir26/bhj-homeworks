8b4f2adc314d0a3b1bed5ceb4983e6fba656bb31
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-10 17:31:01 +0400

    Третье задание

---
+++ event-object/tabs/task.js
@@ -0,0 +1,12 @@
+let tab = Array.from(document.querySelectorAll(".tab"));
+let tabContent = Array.from(document.querySelectorAll(".tab__content"));
+
+for (let t of tab) {
+    t.addEventListener("click", operation);
+}
+
+function operation (event) {
+
+
+}
+
