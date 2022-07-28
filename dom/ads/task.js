6d309145efcaf23ed06efb168fc1e8c16fd16ff5
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-28 18:27:47 +0400

    Сделанное второе задание

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,22 +1,22 @@
 let rotatorCase = Array.from(document.querySelectorAll(".rotator__case"));
 
-
-setInterval(i, 1000);
+let currentRotator = rotatorCase.findIndex((index) => index.classList.contains("rotator__case_active"));
+setTimeout(changeAdd, rotatorCase[currentRotator].dataset.speed);
   
-  function i () {
-    let currentRotator = rotatorCase.findIndex((index) => {return index.classList.contains("rotator__case_active")});
-    let nextRotator;
-
-    if (setInterval(i, 1000)) {
-     nextRotator = currentRotator + 1;
-      if (nextRotator === rotatorCase.length) {
+function changeAdd () {
+    currentRotator = rotatorCase.findIndex((index) => index.classList.contains("rotator__case_active"));
+    let nextRotator = currentRotator + 1;
+      
+    if (nextRotator === rotatorCase.length) {
          nextRotator = 0;
-      }
+    }
 
-}
+rotatorCase[nextRotator].style.color = rotatorCase[nextRotator].dataset.color;
 
 rotatorCase[nextRotator].classList.add("rotator__case_active");
 rotatorCase[currentRotator].classList.remove("rotator__case_active");
+
+setTimeout(changeAdd, rotatorCase[nextRotator].dataset.speed);
 
 }
 
