bcdfd20bc93e307acdf80fd116861c06fb87be6e
vlad_mir <mironow.slava2012@yandex.ru>
2022-10-03 12:53:10 +0400

    Cделал ответробота

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,12 +1,11 @@
 const chat = document.querySelector(".chat-widget");
 
+const ROBOT_MESSAGES = ['Добрый день', 'Все операторы заняты', 'Скоро вернусь', 'Немного подождите', 'Спасибо за вопрос'];
 
 chat.addEventListener("click", openingChat);
 
-function openingChat (event) {
-    if (event.target) {
-       chat.classList.add("chat-widget_active"); 
-    }
+function openingChat () {
+  chat.classList.add("chat-widget_active"); 
 }
 
 const input = document.querySelector(".chat-widget__input");
@@ -18,8 +17,19 @@ input.addEventListener("keydown", sendingMessageUser);
 function sendingMessageUser (event) {
     if (event.key === "Enter") {
      messageClient.innerHTML += `<div class="message_client">
-     <div class="message__time">22:10</div>
-     <div class="message__text">Добрый день!</div>
- </div>`;
+        <div class="message__time">22:10</div>
+        <div class="message__text">${input.value}</div>
+    </div>`;
+    input.value = '';
+    sendingMessageRobot();
+    }
+} 
+
+function sendingMessageRobot () {
+    const randomIndex = Math.floor(Math.random()*ROBOT_MESSAGES.length);
+    messageClient.innerHTML += `<div class="message">
+    <div class="message__time">22:10</div>
+    <div class="message__text">${ROBOT_MESSAGES[randomIndex]}</div>
+</div>`;
 }
-}
+
