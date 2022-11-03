fee64cb67c1fd8c2c8e574bb9edfd144efb17f20
vlad_mir <mironow.slava2012@yandex.ru>
2022-11-03 14:35:30 +0300

    Пытался сделать задание со списком

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -6,7 +6,6 @@ class Autocomplete {
     this.list = container.querySelector( '.autocomplete__list' );
     this.valueContainer = container.querySelector( '.autocomplete__value' );
     this.valueElement = container.querySelector( '.autocomplete__text-content' );
-
     this.registerEvents();
   }
 
@@ -67,27 +66,19 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
+  getMatches(text) {
+     let options = Array.from(this.input.options);
+     let optionText = options.forEach((option) => {option.innerText.includes(text)});
+    
+    
+     
+       return [
       {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+        text: '',
+        value: ''
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    ]};
   }
-}
+
 
 new Autocomplete( document.querySelector( '.autocomplete' ));
