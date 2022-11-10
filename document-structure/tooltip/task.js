1544360877f7421ff866d885f0a32193cc355d2c
vlad_mir <mironow.slava2012@yandex.ru>
2022-11-10 18:13:18 +0300

    Начал делать задание с подсказками

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,9 @@
+const hasTootlip = Array.from(document.querySelectorAll(".has-tooltip"));
+
+for (let el of hasTootlip) {
+    el.addEventListener('click', promptAppearance);
+}
+
+function promptAppearance () {
+
+}    
