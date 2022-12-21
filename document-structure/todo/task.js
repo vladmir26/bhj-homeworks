dd51abeac2ce6f20691b7a6a7742bf803acb77c0
Vlad <mironow.slava2012@yandex.ru>
2022-12-21 15:17:19 +0300

    Добавил возможность не добавлять пустую задачу, не получается удалять задачи

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,28 +3,25 @@ const tasksList = document.querySelector('.tasks__list');
 const taskRemove = document.querySelector('.task__remove');
 const task = document.querySelector('.task');
 
-input.addEventListener("keydown", addingTasks);
-
-function addingTasks (event) {
+input.addEventListener("keydown", (event) => {
 
    if (event.key === "Enter")  {
     event.preventDefault();
-    
-    tasksList.innerHTML += `<div class="task">
-    <div class="task__title">
+
+    if(input.value != '') {
+       tasksList.innerHTML += `<div class="task">
+       <div class="task__title">
       ${input.value}
-    </div>
-    <a href="#" class="task__remove">&times;</a>
-  </div>`;
-  input.value = '';
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+      </div>`;
+      input.value = '';
+   }
 }
-}
+})
 
-taskRemove.addEventListener("click", removingTasks);
-
-
-
-function removingTasks (event) {
-    t.remove();
-}
+taskRemove.addEventListener("click", () => {
+    task.remove();
+    
+})
    
