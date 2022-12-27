f832d3d6315b82b775b3df49af159e699c523721
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2022-12-27 17:00:10 +0300

    Сделанное задание со списком дел

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('.tasks__input');
 const tasksList = document.querySelector('.tasks__list');
-const taskRemove = document.querySelector('.task__remove');
 const task = document.querySelector('.task');
+const tasksControl = document.querySelector('.tasks__control');
 
 input.addEventListener("keydown", (event) => {
 
@@ -16,12 +16,34 @@ input.addEventListener("keydown", (event) => {
       <a href="#" class="task__remove">&times;</a>
       </div>`;
       input.value = '';
+
+      const taskRemove = document.querySelectorAll('.task__remove');
+
+     }
    }
-}
 })
 
-taskRemove.addEventListener("click", () => {
-    task.remove();
-    
+tasksControl.addEventListener("submit", (event) => {
+   event.preventDefault();
+  
+      if(input.value != '') {
+         tasksList.innerHTML += `<div class="task">
+         <div class="task__title">
+        ${input.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+        </div>`;
+        input.value = '';
+  
+        const taskRemove = document.querySelectorAll('.task__remove');
+      }
+   }
+)
+
+tasksList.addEventListener("click", (event) => {
+
+    if (event.target.classList.contains('task__remove')) {
+      event.target.parentNode.remove();
+   }
 })
    
