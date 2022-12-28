1120a401396210412d6eef58069912ede064a40d
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2022-12-28 18:05:10 +0300

    Сделал запрос к серверу и вывел ответ в консоль

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,12 +1,18 @@
 const items = document.getElementById('items');
 
-let xnr = new XMLHttpRequest();
-xnr.open('GET', 'http://netology-slow-rest.herokuapp.com');
-xnr.responseType = 'text';
-xnr.setRequestHeader('Content-Type', 'http://netology-slow-rest.herokuapp.com');
-xnr.send();
-xnr.onreadystatechange = function () {
-    if(xnr.readyState === 4) {
-    console.log(xnr.responseType);
-    };
-    };
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+
+xhr.responseType = 'json';
+
+xhr.onload = () => {
+    console.log(xhr.response);
+}
+
+
+xhr.send();
+
+
+
+
