cbe259569cf69e55c7af37b4797d232bf3fc5705
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-01-02 17:30:10 +0300

    Не получается не добавлять новый элемент в корзину при увеличении количества

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,18 +2,65 @@ const productValue = Array.from(document.querySelectorAll('.product__quantity-va
 const productDec = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
 const productInc = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
 
-let text;
+const products = Array.from(document.querySelector('.product'));
 
-productValue.forEach((element) =>  {
-    
-  text =  Number(element.textContent);
+let productsStore = [];
+
+products.forEach((product) =>  {
+   productsStore.push({
+    id: product.dataset.id,
+    amount: Number(product.textContent)
+   }) 
 });
 
 
 
-productInc.forEach((element) => {element.addEventListener('click', (event) => {
+productInc.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    const plus = event.target;
+    const product = plus.closest('.product');
+    const amount = product.querySelector('.product__quantity-value');
+    amount.textContent = Number(amount.textContent) + 1;
+  }
+)});
 
-    productValue.textContent = text += 1;
-}
-)})
+productDec.forEach((element) => {
+  element.addEventListener('click', (event) => {
+      const minus = event.target;
+      const product = minus.closest('.product');
+      const amount = product.querySelector('.product__quantity-value');
+      
+      if(Number(amount.textContent) > 0) {
+        amount.textContent = Number(amount.textContent) - 1
+      }
+  })
+});
+
+const productAdd = Array.from(document.querySelectorAll('.product__add'));
+
+productAdd.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    const btn = event.target;
+    const product = btn.closest('.product');
+
+    const id = product.dataset.id;
+    const img = product.querySelector('img');
+    const src = img.src;
+    const amount = product.querySelector('.product__quantity-value');
+    const amountTextContent = amount.textContent;
+    
+    const cartProducts = document.querySelector('.cart__products');
+    
+    cartProducts.innerHTML += `<div class="cart__product" data-id="${id}">
+    <img class="cart__product-image" src="${src}">
+    <div class="cart__product-count">${amountTextContent}</div>
+    </div>`;
+
+    cartProducts.forEach((element) => {
+      if(element.id) {
+        element.amountTextContent = Number(amountTextContent) + 1;
+      }
+    })
+  })
+});
 
