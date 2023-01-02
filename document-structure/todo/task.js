5e366eba96250b3fb043c518844c23c30e548497
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-01-02 15:40:59 +0300

    Сделанное задание со списком

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,19 +3,23 @@ const tasksList = document.querySelector('.tasks__list');
 const task = document.querySelector('.task');
 const tasksControl = document.querySelector('.tasks__control');
 
+const addTodo = () => {
+   tasksList.innerHTML += `<div class="task">
+    <div class="task__title">
+   ${input.value}
+   </div>
+   <a href="#" class="task__remove">&times;</a>
+   </div>`;
+   input.value = '';
+};
+
 input.addEventListener("keydown", (event) => {
 
    if (event.key === "Enter")  {
     event.preventDefault();
 
     if(input.value != '') {
-       tasksList.innerHTML += `<div class="task">
-       <div class="task__title">
-      ${input.value}
-      </div>
-      <a href="#" class="task__remove">&times;</a>
-      </div>`;
-      input.value = '';
+      addTodo();
      }
    }
 })
@@ -24,14 +28,7 @@ tasksControl.addEventListener("submit", (event) => {
    event.preventDefault();
   
       if(input.value != '') {
-         tasksList.innerHTML += `<div class="task">
-         <div class="task__title">
-        ${input.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-        </div>`;
-        input.value = '';
-  
+        addTodo();
       }
    }
 )
