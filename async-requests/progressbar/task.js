10668c6b4c7386821f1a835ae3934399eb29b657
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-01-24 18:50:55 +0300

    При нажатии на кнопку отправить шкала заполняется

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,15 +2,18 @@ const progress = document.getElementById('progress');
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
+    event.preventDefault();
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.onload = () => {
         console.log(xhr.response);
     }
+    
 
     xhr.upload.addEventListener('progress', (event) => {
         console.log(event);
+        progress.value = "1";
     })
     xhr.send(formData);
 })
