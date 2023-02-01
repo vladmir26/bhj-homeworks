e464bd80f4aff04b95b3c6777be43a005a74cceb
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-02-01 14:27:25 +0300

    Сделал заданиес текстовым редактором и повышенный уровень

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,13 +1,21 @@
 const editor = document.getElementById('editor');
+const btn = document.querySelector('.btn');
 
+editor.addEventListener("keyup", () => {
 
+ localStorage.setItem("storage",editor.value);
+ localStorage.getItem("storage");
 
-editor.addEventListener("keyup", (event) => {
-    localStorage.setItem('storage',event.key)
+})
 
-    console.log(localStorage.getItem('storage'))
-    
-    
+window.addEventListener("load", () => {
+   if(localStorage.getItem("storage")) {
+      editor.value = localStorage.getItem("storage");
+   }
+})
+
+btn.addEventListener("click", () => {
+   editor.value = '';
 })
 
 
