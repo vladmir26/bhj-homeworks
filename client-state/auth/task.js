80395f90db32e574484e844531d1df4c96ec2d47
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-02-07 20:40:20 +0300

    Сделал задание с авторизацией и повышенный уровень

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('signin__form');
 const userId = document.getElementById('user_id');
 const welcome = document.getElementById('welcome');
+const btnClear = document.getElementById('clear__btn');
+const login = document.getElementById('clear__login');
+const password = document.getElementById('clear__password');
 
 let id;
 
@@ -24,15 +27,21 @@ form.addEventListener('submit', (event) => {
         }
 
     }
-     xhr.send(formData);
+    xhr.send(formData);
 
 
 })
 
-window.addEventListener("load", () => {
-    if(id) {
-       userId.textContent = id;
+window.addEventListener('load', () => {
+    if(localStorage.getItem('id')) {
+       userId.textContent = localStorage.getItem('id');
        welcome.classList.add('welcome_active');  
     }
    })
+
+   btnClear.addEventListener('click', () => {
+    login.value = '';
+    password.value = '';
+  })
+
 
